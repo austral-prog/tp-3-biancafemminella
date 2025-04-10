d8d8833782975aea63932bbbee42cab67d8f23aa
biancafemminella <bfemminella@mail.austral.edu.ar>
2025-04-10 14:55:45 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,12 @@
 def check_vowels():
     # Código a implementar utilizando input.
+	nombre = str(input("Ingrese un nombre:").lower())
+	print (f"Contiene a: {"a" in nombre}")
+	print (f"Contiene e: {"e" in nombre}")
+	print (f"Contiene e: {"i" in nombre}")
+	print (f"Contiene e: {"o" in nombre}")
+	print (f"Contiene e: {"u" in nombre}")
+check_vowels()
 
 
 # Para verificar este ejercicio ejecutar el comando
