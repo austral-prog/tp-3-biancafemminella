15dd6c1cd5e47d8cb0bcd6e965a0081f78399655
biancafemminella <bfemminella@mail.austral.edu.ar>
2025-04-10 14:57:58 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,6 +2,13 @@ def slice_simple():
     texto = "Awesome"
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+	primeras = texto[0:3].lower()
+	medio = texto[2:5].lower()
+	combinado = texto[0:4].lower() + texto[-3:].lower()
+	print(primeras)
+	print(medio)
+	print(combinado)
+slice_simple()
 
 
 # Para verificar este ejercicio ejecutar el comando
